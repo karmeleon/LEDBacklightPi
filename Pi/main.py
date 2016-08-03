24595a54cf20c771d86b911d08038c0b0c78ec6c
Shawn Walton <karmeleonnn@gmail.com>
2016-08-03 20:47:28 +0000

    RPi part theoretically works now

--- Pi/main.py
+++ Pi/main.py
@@ -1,28 +1,42 @@
 import socket
 import pigpio
+import select
+import threading
 
 def main():
 	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	serversocket.bind((socket.gethostname(), 1420))
+	serversocket.bind(("192.168.1.120", 1420))
 	serversocket.listen(5)
+
+	gpio = pigpio.pi()
+
 	while True:
 		clientsocket, address = serversocket.accept()
-		data = receive_data(clientsocket)
-		print(data)
+		t = threading.Thread(target=manage_connection, args=(clientsocket, gpio))
+		t.daemon = True
+		t.start()
 
-def receive_data(clientsocket):
-	chunks = []
-	bytes_recd = 0
-	while bytes_recd < MSGLEN:
-		chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
-		if chunk == b'':
-			raise RuntimeError("socket connection broken")
-		chunks.append(chunk)
-		bytes_recd = bytes_recd + len(chunk)
-	return b''.join(chunks)
+def manage_connection(socket, gpio):
+	socket.setblocking(0)
+	while True:
+		# wait up to 30 seconds for data to become available on the socket
+		ready = select.select([socket], [], [], 30)
+		if ready[0]:
+			data = socket.recv(512)
+			if len(data) == 0:
+				print("Socket closed, ending thread")
+				return
+			else:
+				data = data.decode("utf-8")
+				color = [int(s) for s in data.split(sep=".")]
+				#print(str.format("R:{}, G:{}, B:{}", color[0], color[1], color[2]))
+				set_color(color, gpio)
 
-
-
+def set_color(color, gpio):
+	# R:17, G:22, B:24
+	gpio.set_PWM_dutycycle(17, color[0])
+	gpio.set_PWM_dutycycle(22, color[1])
+	gpio.set_PWM_dutycycle(24, color[2])
 
 
 if __name__ == '__main__':
